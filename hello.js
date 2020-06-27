a1c662315e5447a5a8e0dcf3d70f8c7a80e2e895
mike-hopper-thd <67391844+mike-hopper-thd@users.noreply.github.com>
2020-06-27 21:34:03 +0000

    Adds hello.js

---
+++ hello.js
@@ -0,0 +1,6 @@
+const msg = "Hello from codespaces.";
+console.log(msg);
+
+const numbers = [1, 2, 3, 4, 5];
+const sumOfSquares = numbers.map(n => n * n).reduce((a, b) => a + b, 0);
+console.log(sumOfSquares);
